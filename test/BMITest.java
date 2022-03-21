91fedc66b267e7f74b6a3593ff4562df44c55a36
Mikeskay <47972946+Mikeskay@users.noreply.github.com>
2022-03-21 17:52:42 +0000

    Create BMITest.java

---
+++ test/BMITest.java
@@ -0,0 +1,16 @@
+package bmicalculator.test;
+
+import bmicalculator.base.display.BMICalDisplay;
+import java.util.Scanner;
+
+public class BMITest{
+
+    public static void main(String[] args) {
+
+
+     BMICalDisplay test = new BMICalDisplay();
+        
+    }
+    
+
+}
